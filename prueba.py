818f2e7eac02ea7e214dd808bac7b9f081c3ae9a
kamilodiaz0521@gmail.com <kamilodiaz0521@gmail.com>
2024-09-18 12:35:39 -0500

    codigos de prueba

--- prueba.py
+++ prueba.py
@@ -2,7 +2,7 @@ import time
 
 print("Hola") # esta linea imprime en consola la palabra hola
 print("Hello world \n")
-time.sleep(3)
+time.sleep(2)
 print("segundo reglon")
 
 print(type("hola"))
@@ -10,3 +10,28 @@ print(type(4))
 print(type(5.5))
 
 print("pruebagitignore")
+print("---------------------------")
+bool= True 
+print(bool)
+print(type(bool))
+numero= 5 
+texto= "hola"
+suma= str (numero) + texto
+print(suma)
+
+bool1= True 
+bool2= False 
+
+suma2= bool1 and bool2
+print(suma2)
+
+A= True
+B= True 
+C= True 
+D= True 
+
+Resultado2= suma(A and B and C and D)
+print(Resultado2)
+
+
+
